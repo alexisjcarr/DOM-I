0c7f6a72321db182417be31bcc753f7c90c8e749
alexisjcarr <acarr59@gmail.com>
2019-05-13 13:14:31 -0500

    Changed numbers to monospaced font

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -5,20 +5,21 @@ function timer() {
   let timer = setInterval(function() {
     milliseconds += 10;
     seconds = Math.floor(milliseconds / 1000);
+
     (milliseconds % 1000) / 10 < 10
       ? (document.querySelector(
           ".digits"
-        ).innerHTML = `<h1>0${seconds} : 0${(milliseconds % 1000) / 10}</h1>`)
+        ).innerHTML = `<h1>0${seconds}:0${(milliseconds % 1000) / 10}</h1>`)
       : (document.querySelector(
           ".digits"
-        ).innerHTML = `<h1>0${seconds} : ${(milliseconds % 1000) / 10}</h1>`);
+        ).innerHTML = `<h1>0${seconds}:${(milliseconds % 1000) / 10}</h1>`);
 
     if (seconds === 10) {
       document.querySelector(
         ".digits"
-      ).innerHTML = `<h1>${seconds} : ${(milliseconds % 1000) / 10}0</h1>`;
+      ).innerHTML = `<h1>${seconds}:0${(milliseconds % 1000) / 10}</h1>`;
 
-      document.querySelector(".digits").style.color = "red";
+      document.querySelector(".digits").classList.add('redDigit');
 
       clearInterval(timer);
     }
